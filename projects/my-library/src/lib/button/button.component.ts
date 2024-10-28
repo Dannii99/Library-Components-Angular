ca553b7da9ae14084de6f88f01416e9e0bdf1f96
Danny Ospino <sistemas17@red5g.co>
2024-10-28 18:45:28 -0500

    estructura

---
+++ projects/my-library/src/lib/button/button.component.ts
@@ -0,0 +1,23 @@
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'lib-button',
+  standalone: true,
+  imports: [],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+  @Input() label!: string;
+  @Input() severity!:string;
+  @Input() icon!:string;
+  @Input() iconPos: 'right' | 'left' = 'left';
+  @Input() link:boolean = false;
+  @Input() disabled:boolean = false;
+  @Input() raised:boolean = false;
+  @Input() rounded:boolean = false;
+  @Input() typeText:boolean = false;
+  @Input() outline:boolean = false;
+  @Input() text:boolean = false;
+
+}
